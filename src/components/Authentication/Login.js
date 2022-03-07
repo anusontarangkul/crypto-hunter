45b09fbcb550d9635582d5b692515778f0f0bb11
David Anusontarangkul <anusontarangkul.d@gmail.com>
2022-03-07 10:51:22 -0800

    add login component

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box, TextField, Button } from '@material-ui/core'
 
 const Login = ({ handleClose }) => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState('')
+    const handleSubmit = () => { }
     return (
         <div>
-            login
+            <Box
+                p={3}
+                style={{ display: "flex", flexDirection: "column", gap: '20px' }}
+            >
+                <TextField
+                    variant="outlined"
+                    type="email"
+                    label="Enter Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    fullWidth
+                >
+                </TextField>
+                <TextField
+                    variant="outlined"
+                    type="password"
+                    label="Enter Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    fullWidth
+                >
+                </TextField>
+
+                <Button
+                    variant="contained"
+                    size="large"
+                    style={{ backgroundColor: "#EEBC1D" }}
+                    onClick={handleSubmit}
+                >Login</Button>
+            </Box>
+
         </div>
     )
 }
