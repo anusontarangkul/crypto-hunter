e6d0a149e12edcb2c3e2683ae47dcb716ed0d3b3
David Anusontarangkul <anusontarangkul.d@gmail.com>
2022-03-07 10:04:39 -0800

    config auth

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCm9ZswTJugFqI5uoaF3Ypws_jXJBQJ5IY",
+    authDomain: "crypto-hunter-df280.firebaseapp.com",
+    projectId: "crypto-hunter-df280",
+    storageBucket: "crypto-hunter-df280.appspot.com",
+    messagingSenderId: "96573453707",
+    appId: "1:96573453707:web:6e9686d23a81e208e1eb8e"
+};
