17c1db087474be522f7ef5fa3e2aeb53cf28535b
David Anusontarangkul <anusontarangkul.d@gmail.com>
2022-03-07 09:57:15 -0800

    refactored fetch to context

--- src/CryptoContext.js
+++ src/CryptoContext.js
@@ -1,17 +1,29 @@
 import React, { createContext, useState, useContext, useEffect } from 'react'
-
+import axios from 'axios'
+import { CoinList } from './config/api'
 const Crypto = createContext()
 
 const CryptoContext = ({ children }) => {
+    const [coins, setCoins] = useState([])
+    const [loading, setLoading] = useState(false)
     const [currency, setCurrency] = useState("USD")
     const [symbol, setSymbol] = useState('$')
+
+    const fetchCoins = async () => {
+        setLoading(true)
+        const { data } = await axios.get(CoinList(currency))
+
+        setCoins(data)
+        setLoading(false)
+
+    }
 
     useEffect(() => {
         if (currency === 'USD') setSymbol('$')
         else if (currency === 'INR') setSymbol('₹')
     }, [currency])
     return (
-        <Crypto.Provider value={{ currency, symbol, setCurrency }}>
+        <Crypto.Provider value={{ currency, symbol, setCurrency, coins, loading, fetchCoins }}>
             {children}
         </Crypto.Provider>
     )
